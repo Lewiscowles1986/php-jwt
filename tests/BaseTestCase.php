fe37227cd18e6a9504a1915873d8391699f1e0db
Lewis Cowles <lewiscowles@me.com>
2021-03-27 05:58:00 +0000

    Prep: BaseTestCase class

---
+++ tests/BaseTestCase.php
@@ -0,0 +1,22 @@
+<?php
+namespace Tests;
+
+use PHPUnit\Framework\TestCase;
+
+class BaseTestCase extends TestCase
+{
+    /*
+     * For compatibility with PHPUnit 4.8 and PHP < 5.6
+     */
+    public function setExpectedException($exceptionName, $message = '', $code = null)
+    {
+        if (method_exists($this, 'expectException')) {
+            $this->expectException($exceptionName);
+            if ($message) {
+                $this->expectExceptionMessage($message);
+            }
+        } else {
+            parent::setExpectedException($exceptionName, $message, $code);
+        }
+    }
+}
