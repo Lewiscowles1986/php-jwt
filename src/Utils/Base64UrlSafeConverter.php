d31408a45fa7a3e9aca97ef07e30248e25a5a4c7
Lewis Cowles <lewiscowles@me.com>
2021-03-27 09:22:54 +0000

    Extract: Base64UrlConverter

---
+++ src/Utils/Base64UrlSafeConverter.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Firebase\JWT\Utils;
+
+class Base64UrlSafeConverter
+{
+    /**
+     * Decode a string with URL-safe Base64.
+     *
+     * @param string $input A Base64 encoded string
+     *
+     * @return string A decoded string
+     */
+    public static function decode($input)
+    {
+        $remainder = \strlen($input) % 4;
+        if ($remainder) {
+            $padlen = 4 - $remainder;
+            $input .= \str_repeat('=', $padlen);
+        }
+        return \base64_decode(\strtr($input, '-_', '+/'));
+    }
+
+    /**
+     * Encode a string with URL-safe Base64.
+     *
+     * @param string $input The string you want encoded
+     *
+     * @return string The base64 encode of what you passed in
+     */
+    public static function encode($input)
+    {
+        return \str_replace('=', '', \strtr(\base64_encode($input), '+/', '-_'));
+    }
+}
