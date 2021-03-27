e4bc11b63ce568ce11169aab9b1fa1b07f4582cb
Lewis Cowles <lewiscowles@me.com>
2021-03-27 09:22:31 +0000

    Extract: StringUtils

---
+++ src/Utils/StringUtils.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Firebase\JWT\Utils;
+
+class StringUtils
+{
+    /**
+     * Get the number of bytes in cryptographic strings.
+     *
+     * @param string $str
+     *
+     * @return int
+     */
+    public static function safeStrlen($str)
+    {
+        if (\function_exists('mb_strlen')) {
+            return \mb_strlen($str, '8bit');
+        }
+        return \strlen($str);
+    }
+}
